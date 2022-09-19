0bc1e8e13442e2a70cda9b9637bc2288fd839360
FacuSalco <facusalco26@gmail.com>
2022-09-19 10:52:43 -0300

    Update 19/9
    Hice hasta el 3

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+//https://github.com/nachovigilante/avanzado-bases-de-datos/tree/main/restaurant
+
 const express = require('express');
 const app = express();
 const port = 9000;
+const menu = require('./menu.json');
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -8,4 +12,16 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`>Server running on port ${port}`);
+});
+
+// Crear un endpoint GET /menu que devuelva el menú completo del restaurante.
+app.get('/menu', (req, res) => {
+  res.json (menu);
+});
+
+// Crear un endpoint GET /menu/:id que devuelva un plato del menú. El id del plato debe ser pasado como parámetro en la ruta.
+app.get('/menu/:id', (req, res) => {
+  const id = req.params.id;
+  const plato = menu.find((plato) => plato.id === id);
+  res.json(plato);
 });
